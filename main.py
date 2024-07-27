b6835c83ab845bfca7ab998ca3d8e0d45f50d741
HLoc26 <dhl26052004@gmail.com>
2024-07-27 11:31:46 +0700

    get collections links

--- main.py
+++ main.py
@@ -9,7 +9,20 @@ def main():
     driver = webdriver.Edge()
     url = 'https://memoryzone.com.vn/'
     driver.get(url)
-    
+    link_product = []
+    product_types = driver.find_elements(By.CSS_SELECTOR, ".item.slick-slide [href]")
+    for type in product_types:
+        link = type.get_attribute("href")
+        if link not in link_product:
+            link_product.append(link)
+    collections = []
+    for link in link_product[1:3]:
+        driver.get(link)
+        print(link)
+        sleep(5)
+        link = driver.current_url
+        print(link)
+
 
 if __name__ == "__main__":
     main()
