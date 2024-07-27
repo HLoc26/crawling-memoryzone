7275b23eefcb5caf69c89f27589b63db50571e47
HLoc26 <dhl26052004@gmail.com>
2024-07-28 02:45:07 +0700

    Get items' ratings

--- main.py
+++ main.py
@@ -18,16 +18,16 @@ def GetText(elems: list) -> list:
 
 def GetItemInfoInPage(page: int) -> list[object]: # [[item1], [item2], [item3]]
     items = []
-    # Get name
+    # === Get name ===
     names_elems = driver.find_elements(By.CSS_SELECTOR, ".product-name")
     names = GetText(names_elems)
-    print(names)
-    # Get current price (discounted price)
+    # print(names)
+    # === Get current price (discounted price) ===
     curr_prices_elems = driver.find_elements(By.CSS_SELECTOR, ".price, .price-contact")
     curr_prices = GetText(curr_prices_elems)
-    print(curr_prices)
+    # print(curr_prices)
 
-    # Get original price (before discount)
+    # === Get original price (before discount) ===
     price_box_elems = driver.find_elements(By.CSS_SELECTOR, ".price-box")
     original_prices = []
     for elem in price_box_elems:
@@ -37,22 +37,37 @@ def GetItemInfoInPage(page: int) -> list[object]: # [[item1], [item2], [item3]]
         except NoSuchElementException:
             original_prices.append("")
             
-    # Get link
+    # === Get link ===
     link_elems = driver.find_elements(By.CSS_SELECTOR, ".product-name [href]")
     links = []
     for elem in link_elems:
         link = elem.get_attribute("href").strip()
         links.append(link)
-    print(links)
-    print(f"names: {len(names)} | cur: {len(curr_prices)} | ori: {len(original_prices)} | links: {len(links)}")
+    # print(links)
+    # === Get ratings ===
+    ratings = []
+    rating_elems = driver.find_elements(By.CSS_SELECTOR, ".aer-review-result")
+    for elem in rating_elems:
+        stars = elem.find_elements(By.XPATH, ".//span[@class='aer-star-active']")
+        print(stars)
+        ratings.append(len(stars))
+    # print(ratings)
+    # === Get rate count ===
+    rate_count = []
+    rate_elems = driver.find_elements(By.CSS_SELECTOR, ".aer-review-result")
+    for elem in rate_elems:
+        elem_class = elem.get_attribute("class")
+        total_count = elem_class.split()[1].strip("ae-total-")
+        if total_count:
+            count = int(total_count)
+        else:
+            count = 0
+        rate_count.append(count)
+    # print(len(rate_count), rate_count)
+    # print(f"names: {len(names)} | cur: {len(curr_prices)} | ori: {len(original_prices)} | links: {len(links)}")
     assert len(names) == len(curr_prices) == len(original_prices) == len(links), "ERROR: len not match"
     for i in range(len(names)):
-        item = Item(names[i],\
-                    curr_prices[i], \
-                    original_prices[i], \
-                    links[i], "5", \
-                    "10", \
-                    [])
+        item = Item(names[i],curr_prices[i], original_prices[i], links[i], ratings[i], rate_count[i])
         items.append(item)
     return items
 
@@ -60,6 +75,7 @@ def GetItemCollection(link: str) -> Collection:
     # Get title
     title = driver.find_element(By.CSS_SELECTOR, ".title_page").text
     collection = Collection(title)
+    # Get items on first page
     page1 = Page(1)
     page1.AddItem(GetItemInfoInPage(1))
     collection.AddPage(page1)
@@ -84,8 +100,7 @@ def GetItemCollection(link: str) -> Collection:
         collection.AddPage(page)
     return collection
 
-
-def main():
+def GetCollectionLinks() -> list[str]:
     link_product = []
     product_types = driver.find_elements(By.CSS_SELECTOR, ".item.slick-slide [href]")
     for type in product_types:
@@ -98,18 +113,26 @@ def main():
             link = type.get_attribute("href")
             if link not in link_product:
                 link_product.append(link)
-    collections = []
-    for link in link_product[1:]:
-        driver.get(link)
-        print(link)
-        sleep(5)
-        link = driver.current_url
-        print(link)
-        collections.append(GetItemCollection(link))
-    count = 0
-    for collection in collections:
-        count += collection.GetPageCount()
-    print(count)
+    return link_product
+
+
+
+def main():
+    link_collections = GetCollectionLinks()
+    # collections = []
+    # for link in link_collections[1:]:
+    #     driver.get(link)
+    #     print(link)
+    #     sleep(5)
+    #     link = driver.current_url
+    #     print(link)
+    #     collections.append(GetItemCollection(link))
+    # count = 0
+    # for collection in collections:
+    #     count += collection.GetPageCount()
+    # print(count)
+
+    driver.get(link_collections[1])
 
 if __name__ == "__main__":
     main()
