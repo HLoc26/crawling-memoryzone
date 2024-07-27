c6367b2c37f06c789faa2ff33ca85aaafdc7bc23
HLoc26 <dhl26052004@gmail.com>
2024-07-27 17:11:24 +0700

    Update classes

--- PageStructure.py
+++ PageStructure.py
@@ -7,8 +7,32 @@ class Item:
         self.rating = rating
         self.rate_count = rate_count
         self.feat = feat
+    def __str__(self) -> str:
+        if self.original_price == "":
+            original = "Not on discount"
+        else:
+            original = self.original_price
+        features = '\t'.join(self.feat)
+        info =  f"Item name: {self.name}\nCurrent price: {self.curr_price}\n" + \
+                f"Origianl price: {original}\nLink: {self.link}\n" + \
+                f"Rating: {self.rating}\nRate Count: {self.rate_count}\n" + \
+                f"Features: \n\t{features}"
+        return info
+
+    def __repr__(self) -> str:
+        if self.original_price == "":
+            original = "Not on discount"
+        else:
+            original = self.original_price
+        features = '\t'.join(self.feat)
+        info =  f"\nItem name: {self.name}\nCurrent price: {self.curr_price}\n" + \
+                f"Origianl price: {original}\nLink: {self.link}\n" + \
+                f"Rating: {self.rating}\nRate Count: {self.rate_count}\n" + \
+                f"Features: \n\t{features}\n"
+        return info
 
 class Page:
+    items = list[Item]
     def __init__(self, index: int) -> None:
         self.index: int = index
         self.items: list[Item] = []
@@ -16,6 +40,7 @@ class Page:
         self.items.append(item)
 
 class Collection:
+    pages = list[Page]
     def __init__(self, title: str) -> None:
         self.title = title
         self.pages: list[Page] = []
