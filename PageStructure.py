f22d14a653998f879d1a28d6390de8f731f103cd
HLoc26 <dhl26052004@gmail.com>
2024-07-28 04:54:31 +0700

    Update Item: add condition for discount

--- PageStructure.py
+++ PageStructure.py
@@ -1,28 +1,23 @@
+import pandas as pd
 class Item:
     def __init__(self, name, curr_price, original_price, link, rating, rate_count) -> None:
         self.name = name
         self.curr_price = curr_price
-        self.original_price = original_price
         self.link = link
         self.rating = rating
         self.rate_count = rate_count
-    def __str__(self) -> str:
-        if self.original_price == "":
-            original = "Not on discount"
+        if original_price == "":
+            self.original_price = "Not on discount"
         else:
-            original = self.original_price
+            self.original_price = original_price
+    def __str__(self) -> str:
         info =  f"Item name: {self.name}\nCurrent price: {self.curr_price}\n" + \
-                f"Origianl price: {original}\nLink: {self.link}\n" + \
+                f"Origianl price: {self.original_price}\nLink: {self.link}\n" + \
                 f"Rating: {self.rating}\nRate Count: {self.rate_count}\n"
         return info
-
     def __repr__(self) -> str:
-        if self.original_price == "":
-            original = "Not on discount"
-        else:
-            original = self.original_price
         info =  f"\nItem name: {self.name}\nCurrent price: {self.curr_price}\n" + \
-                f"Origianl price: {original}\nLink: {self.link}\n" + \
+                f"Origianl price: {self.original_price}\nLink: {self.link}\n" + \
                 f"Rating: {self.rating}\nRate Count: {self.rate_count}\n"
         return info
 
@@ -59,3 +54,18 @@ class Collection:
         for page in self.pages:
             count += page.GetItemCount()
         return count
+    def ToDataFrame(self) -> pd.DataFrame:
+        data = []
+        for page in self.pages:
+            for item in page.items:
+                data.append({
+                    'Name': item.name,
+                    'Current price': item.curr_price,
+                    'Original price': item.original_price,
+                    'Link': item.link,
+                    'Rating': item.rating,
+                    'Rate Count': item.rate_count
+                })
+        df = pd.DataFrame(data)
+        return df
+        
